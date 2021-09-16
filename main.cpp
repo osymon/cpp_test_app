bb14201a696eeb1ec528839608c30ed56f52146d
Ilya <spline@protonmail.ch>
2021-09-16 17:15:58 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string>
+using namespace std;
+
+
+int main() {
+    ifstream f("config.txt");
+    string str;
+    string delimiter = "Greeting = ";
+    size_t pos = 0;
+    string token;
+
+    if(f) {
+        ostringstream ss;
+        ss << f.rdbuf();
+        str = ss.str();
+
+        while ((pos = str.find(delimiter)) != string::npos) {
+            token = str.substr(0, pos);
+            str.erase(0, pos + delimiter.length());
+        }
+    }
+
+    cout << str;
+}
